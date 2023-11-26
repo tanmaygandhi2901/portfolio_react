0ebe575fb6a7fdb50d103dd75c1e0906624d2570
Tanmay Gandhi <tanmaygandhi@Tanmays-MacBook-Air.local>
2023-11-27 01:32:24 +0530

    Resolved content overriding issues

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -91,7 +91,7 @@ const experienceData = [
 ]
     
   return (
-    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen min-h-max'>
+    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black min-h-screen w-full h-max'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div >
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>
