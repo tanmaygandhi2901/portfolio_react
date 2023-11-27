311b4e7d3e3fb28544790f0280e935b2f37fd914
Tanmay Gandhi <tanmaygandhi@Tanmays-MacBook-Air.local>
2023-11-27 18:15:44 +0530

    Updated form submission reset issue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Contact = () => {
+
+const formRef = useRef();
+
+const handleSubmit = async(e) => {
+    try {
+        const formData = new FormData(formRef.current);
+        await fetch("https://getform.io/f/cf494065-8ed1-45be-8dc6-c29364a7c47e", {
+            method: "POST",
+            body: formData
+        })
+        formRef.current.reset();
+    } catch(e) {
+        console.error(e);
+    }
+}
+
   return (
     <div name="contact" className='min-h-screen w-full h-max bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
         <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
@@ -9,11 +25,11 @@ const Contact = () => {
                 <p className='py-6'>Submit the below to get in touch with me!</p>
             </div>
             <div className='flex justify-center items-center'>
-                <form action="https://getform.io/f/cf494065-8ed1-45be-8dc6-c29364a7c47e" method='POST' className='flex flex-col w-full md:w-1/2'>
+                <form action="https://getform.io/f/cf494065-8ed1-45be-8dc6-c29364a7c47e" method='POST' ref={formRef} onSubmit={handleSubmit} className='flex flex-col w-full md:w-1/2'>
                     <input type="text" name='name' placeholder='Enter your nane' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
                     <input type="email" name='email' placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
                     <textarea name='message' rows={10} className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' placeholder='Enter your message' />
-                    <button className='text-white mt-4 bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3  mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
+                    <button className='text-white mt-4 bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
                 </form>
             </div>
         </div>
